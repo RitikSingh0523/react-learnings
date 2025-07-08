5b8fdf7925eb53df3ecad2f108a101ac9a83c02a
RitikSingh0523 <ritiksingh0523@gmail.com>
2025-07-08 22:59:11 +0530

    React Project Setup

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-const parent = React.createElement("div", { id: "parent" }, [
-    [React.createElement("div", { id: "child1" }, [React.createElement("h1", { id: "heading1" }, "I am h1 tag"), React.createElement("h2", { id: "heading2" }, "I am h2 tag")]), React.createElement("div", { id: "child2" }, [React.createElement("h1", { id: "heading1" }, "I am h1 tag"), React.createElement("h2", { id: "heading2" }, "I am h2 tag")])]
-])
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading =<h1>Welcome to React</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent)
+root.render(heading)
